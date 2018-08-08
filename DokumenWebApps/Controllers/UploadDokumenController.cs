9cdc9b8aae7073f329e354fa0cff552453489310
erikur <erick@actual-training.com>
2018-08-08 17:13:57 +0700

    upload doc

--- DokumenWebApps/Controllers/UploadDokumenController.cs
+++ DokumenWebApps/Controllers/UploadDokumenController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Microsoft.WindowsAzure.Storage;
@@ -27,6 +28,32 @@ namespace DokumenWebApps.Controllers
             blobClient = storageAccount.CreateCloudBlobClient();
             container = blobClient.GetContainerReference("dokumen");
         }
+
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+            await container.CreateIfNotExistsAsync();
+            string namaBaru = string.Empty;
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    namaBaru = Guid.NewGuid() + "-" + DateTime.Now.Year.ToString() +
+                DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + "-" + formFile.FileName;
+                    CloudBlockBlob blockBlob = container.GetBlockBlobReference(namaBaru);
+                    await blockBlob.UploadFromStreamAsync(formFile.OpenReadStream());
+                }
+            }
+            return Content("File " + namaBaru + " berhasil diupload !");
+        }
+
         public IActionResult Index()
         {
             return View();
